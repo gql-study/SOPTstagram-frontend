86f2ed53ee3093ac7c31053351ca21d398bb6e03
nahee-park <jessica396@ewhain.net>
2022-06-13 20:36:09 +0900

    dev: 초기세팅

---
+++ src/pages/index.tsx
@@ -0,0 +1,31 @@
+import styled from '@emotion/styled';
+import type { NextPage } from 'next';
+import Head from 'next/head';
+
+const Home: NextPage = () => {
+  return (
+    <div>
+      <Head>
+        <title>SOPTstagram</title>
+        <meta name="description" content="SOPTstagram" />
+        <link rel="icon" href="/favicon.ico" />
+      </Head>
+
+      <Styled.Main>
+        <h1>메인페이지입니다</h1>
+      </Styled.Main>
+    </div>
+  );
+};
+
+export default Home;
+
+const Styled = {
+  Main: styled.main`
+    display: flex;
+    position: absolute;
+    width: 100px;
+    height: 100px;
+    color: ${({ theme }) => theme.colors.mainColor};
+  `,
+};
