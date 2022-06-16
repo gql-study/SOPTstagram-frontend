82e9556d5039157927961367ba0c8c2f18fbb530
SNUPI <joohaem@daum.net>
2022-06-16 17:41:51 +0900

    feat: make up header section

--- src/components/myPage/HeaderSection.tsx
+++ src/components/myPage/HeaderSection.tsx
@@ -1,3 +1,73 @@
+import styled from '@emotion/styled';
+import OptionIcon from '@src/assets/myPage/OptionIcon';
+
 export default function HeaderSection() {
-  return <div>HeaderSection</div>;
+  const nickname = 'joamjoam_2';
+
+  return (
+    <StSection>
+      <StProfileImg />
+      <StProfileTextWrapper>
+        <StName>
+          {nickname}
+          <OptionIcon />
+        </StName>
+        <StEditBtn type="button">프로필 편집</StEditBtn>
+      </StProfileTextWrapper>
+    </StSection>
+  );
 }
+
+const StSection = styled.section`
+  display: flex;
+
+  padding: 16px;
+`;
+
+const StProfileImg = styled.div`
+
+  margin-right: 28px;
+
+  border-radius: 50%;
+
+  background-color: blue;
+  width: 77px;
+  height: 77px;
+
+  &:hover {
+    cursor: pointer;
+  }
+`;
+
+const StProfileTextWrapper = styled.div`
+  display: flex;
+  flex-direction: column;
+`;
+
+const StName = styled.h2`
+  margin: 0 0 12px;
+  line-height: 32px;
+
+  font-size: 28px;
+  font-weight: 300;
+`;
+
+const StEditBtn = styled.button`
+
+  display: flex;
+  align-items: center;
+  justify-content: center;
+
+  border: 1px solid rgb(219, 219, 219);
+  border-radius: 4px;
+
+  background-color: transparent;
+  width: 250px;
+  height: 30px;
+
+  font-size: 14px;
+
+  &:hover {
+    cursor: pointer;
+  }
+`;
--- src/components/myPage/InfoSection.tsx
+++ src/components/myPage/InfoSection.tsx
@@ -1,3 +1,5 @@
+import styled from '@emotion/styled';
+
 export default function InfoSection() {
-  return <div>InfoSecion</div>;
+  return <div>InfoSection</div>;
 }
