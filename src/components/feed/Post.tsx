33dfc5d13b3e2d3e16f94f4bb6adc7c1bf8f2abe
jynam <njy1007@gmail.com>
2022-06-17 22:56:02 +0900

    feat: 피드 구조 잡기

---
+++ src/components/feed/Post.tsx
@@ -0,0 +1,12 @@
+import styled from '@emotion/styled';
+
+export default function Post() {
+  return <Styled.Wrapper>게시글</Styled.Wrapper>;
+}
+
+const Styled = {
+  Wrapper: styled.div`
+    width: 100vw;
+    height: 660px;
+  `,
+};
---
+++ src/components/feed/PostList.tsx
@@ -0,0 +1,14 @@
+import Post from './Post';
+
+export default function PostList() {
+  return (
+    <div>
+      <Post />
+      <Post />
+      <Post />
+      <Post />
+      <Post />
+      <Post />
+    </div>
+  );
+}
